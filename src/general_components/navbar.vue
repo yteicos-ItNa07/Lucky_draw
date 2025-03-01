4770aa66822a6eb0152645accfac31b885aed04b
chauxin <chauxxin@gmail.com>
2025-03-01 21:26:38 +0800

    新增了關於我們

--- src/general_components/navbar.vue
+++ src/general_components/navbar.vue
@@ -2,7 +2,7 @@
   <div>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">首頁</a>
+        <a class="navbar-brand" href="../../index.html">首頁</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -10,7 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" href="#">關於我們</a>
+              <a class="nav-link" href="../../about-us.html">關於我們</a>
             </li>
           </ul>
         </div>
--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import main from './main_components/main.vue'
 import participantslist from './main_components/participantslist.vue'
 import luckydraw from './main_components/luckydraw.vue'
 import navbar from "./general_components/navbar.vue"
+import aboutUsCard from "./about-us-components/card.vue"
 
 createApp(main).mount('#main')
 createApp(participantslist).mount('#participantslist')
 createApp(luckydraw).mount('#luckydraw')
 createApp(navbar).mount('#navbar')
+createApp(aboutUsCard).mount('#aboutUsCard')
 
