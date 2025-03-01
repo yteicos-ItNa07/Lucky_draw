1d0fcbb594b610adc48bb97ddcca806cd57342b8
chauxin <chauxxin@gmail.com>
2025-03-01 20:44:38 +0800

    規範化了文件分佈

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import main from './main.vue'
-import participantslist from './participantslist.vue'
-import luckydraw from './luckydraw.vue'
-import navbar from "./navbar.vue"
+import main from './main_components/main.vue'
+import participantslist from './main_components/participantslist.vue'
+import luckydraw from './main_components/luckydraw.vue'
+import navbar from "./general_components/navbar.vue"
 
 createApp(main).mount('#main')
 createApp(participantslist).mount('#participantslist')
